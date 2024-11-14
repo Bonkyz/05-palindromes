0413558a2534d492dc750ce357b135ad8c6eb8a9
Bonkyz <119861308+Bonkyz@users.noreply.github.com>
2024-11-14 10:37:55 +0000

    commit palyndrom exercise by Damien Pham

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #### Fonction secondaire
-
+"""class that display palyndroms"""
 
 def ispalindrome(p):
-
+    """module verifying if the string is a palyndrome"""
     # votre code ici
-    
-    return False
-
-#### Fonction principale
-
-
+    p=p.lower()
+    normal = str.maketrans("àâäéèêëîïôöùûüç", "aaaeeeeiioouuuc",  " !'()*,.:;?`- ")
+    new_p = p.translate(normal)
+    return bool(new_p == new_p[::-1])
+### Fonction principale
 def main():
-
+    """module printing the word and telling if it is a palyndrome or not"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
